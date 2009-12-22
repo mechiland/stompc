3fb70ead11cc37e087475e853b95a0412c9be0e3
Huang Liang <exceedhl@gmail.com>
2009-12-22 23:48:18 +0800

    added socket spike

---
+++ spike/error_handle.h
@@ -0,0 +1,8 @@
+#ifndef __ERROR_HANDLE_H__
+#define __ERROR_HANDLE_H__
+
+void die_with_user_message(const char *message, const char *detail);
+void die_with_system_message(const char *message);
+
+#endif /* end of include guard: __ERROR_HANDLE_H__ */
+                         
