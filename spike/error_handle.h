3f267c99b7e3e275d331f85863d60e6a41c24095
Huang Liang <exceedhl@gmail.com>
2010-01-20 00:30:03 +0800

    removed spike dir

--- spike/error_handle.h
+++
@@ -1,8 +0,0 @@
-#ifndef ERROR_HANDLE_H
-#define ERROR_HANDLE_H
-
-void die_with_user_message(const char *message, const char *detail);
-void die_with_system_message(const char *message);
-
-
-#endif /* end of include guard: ERROR_HANDLE_H */
