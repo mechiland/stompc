7442afaf65ddae6c6b4dca88adae326dc80cf084
Huang Liang <exceedhl@gmail.com>
2010-01-21 14:03:40 +0800

    refactored stomp frame parsing

--- src/stomp_frame.c
+++ src/stomp_frame.c
@@ -4,51 +4,58 @@
 #include <stdio.h>
 #include "stomp_frame.h"
 
+
+static int is_valid_frame(int frame_end, int header_end, int verb_end);
+	
 stomp_frame stomp_frame_create(uint8_t *buf, int size)
-{           
-	char *s = malloc((size + 1) * sizeof(char));
-	memcpy(s, buf, size);
-	s[size] = 0;
-	
-	if (strlen(s) == size) {
-		/* does not contain frame delimiter */
-		return NULL;
-	}  
-	
-	int i, body_start = -1;
-	int has_body = 0;
-	for (i = 0; i < strlen(s); i++) {
-		if (s[i] == '\n') {
-			if (i - body_start == 1) {
-				has_body = 1;
-				body_start = i;
-				break;
-			}         
-			body_start = i;
-		}
-	}                                  
-	
-	if (!has_body) {
-		return NULL;
-	}               
-	
-	stomp_frame f = malloc(sizeof(*f));
-	f->body = malloc((strlen(s) - body_start) * sizeof(char));
-	memcpy(f->body, s + body_start + 1, strlen(s) - body_start -1);
-	f->body[strlen(s) - body_start] = 0;
-	
-	for (i = 0; i < strlen(s); i++) {
-		if (s[i] == '\n') {
-			f->verb = malloc((i + 1) * sizeof(char));
-			memcpy(f->verb, s, i);
-			f->verb[i] = 0;
+{   
+	int frame_end = -1;   
+	int header_end = -1;
+	int verb_end = -1; 
+	int line_end = -1;
+	int line_count = 0;
+	int i = 0;
+	uint8_t next_char;
+	while (i < size) {
+		next_char = buf[i];
+		if (next_char == '\0') {
+			frame_end = i;
 			break;
 		}
+		if (next_char == '\n') {   
+			if (line_end >= 0 && i - line_end == 1) {
+				header_end = i;
+			}
+			line_end = i;
+			line_count++;
+		}                
+		if (line_count == 1) {
+			verb_end = line_end;
+		}
+		i++;
+	} 
+
+	if (!is_valid_frame(frame_end, header_end, verb_end)) {
+		return NULL;
 	}
-	
+	     
+	stomp_frame f = malloc(sizeof(*f));
+	f->verb = malloc((verb_end + 1) * sizeof(char));
+	memcpy(f->verb, buf, verb_end);
+	f->verb[verb_end] = 0;
+	                              
+	int body_start = (header_end + 1);
+	int body_len = frame_end - body_start;
+	f->body = malloc((body_len + 1) * sizeof(char));
+	memcpy(f->body, buf + body_start, body_len);
+	f->body[body_len] = 0;
 	
 	return f;
-}              
+}         
+
+static int is_valid_frame(int frame_end, int header_end, int verb_end) {
+	return frame_end >= 0 && header_end >= 0 && verb_end >= 0;
+}     
 
 uint8_t *stomp_frame_serialize(stomp_frame f, int *size)
 {
--- ut/stomp_frame_test.cpp
+++ ut/stomp_frame_test.cpp
@@ -1,53 +1,58 @@
 #include <UnitTest++.h> 
-#include "../src/stomp_frame.h"
+#include "../src/stomp_frame.h"    
+
+stomp_frame create_frame(char *data, int size);
+void check_null_frame(char *data, int size);
 
 TEST(should_return_null_if_buf_is_empty)
 {                                             
-	uint8_t buf[0];
-	stomp_frame f = stomp_frame_create(buf, 0);
-
-	CHECK_EQUAL((stomp_frame)NULL, f);
+	char *data = "";
+	check_null_frame(data, strlen(data));                       
 }
 
 TEST(should_return_null_if_buf_not_contain_stomp_frame)
-{                           
-	char *buf = "not stomp frame";        
-	int len = strlen(buf);
-	uint8_t *b = (uint8_t *)malloc(len * sizeof(*b));
-	memcpy(b, buf, len);
-
-	stomp_frame f = stomp_frame_create(b, len);
-
-	CHECK_EQUAL((stomp_frame)NULL, f);                 
-	free(b);
-}                                     
+{                    
+	char *data = "not stomp frame";
+	check_null_frame(data, strlen(data));                       
+}                   
 
 TEST(should_return_null_if_buf_is_not_valid_stomp_frame)
 {
-	char *buf = "invalid stomp frame\n\0abc";
-	int len = strlen(buf) + 4;
-	uint8_t *b = (uint8_t *)malloc(len * sizeof(*b));
-	memcpy(b, buf, len);
+	char *data = "invalid stomp frame\n\0abc";
+	check_null_frame(data, strlen(data) + 4);                       
+	
+	data = "\0";
+	check_null_frame(data, strlen(data) + 1);                       
 
-	stomp_frame f = stomp_frame_create(b, len);
-
-	CHECK_EQUAL((stomp_frame)NULL, f);	     
-
-	free(b);
+	data = "a\nb\n\0";
+	check_null_frame(data, strlen(data) + 1);                       
+	
 }
 
 TEST(should_return_frame_if_buf_contain_valid_stomp_frame)
 {                                                        
-	char *buf = "CONNECT\n\n\0";                           	
-	int len = strlen(buf) + 1;
-	uint8_t *b = (uint8_t *)malloc(len * sizeof(*b));
-	memcpy(b, buf, len);
-
-	stomp_frame f = stomp_frame_create(b, len);
-
+	char *data = "CONNECT\n\naaa\0";                           	
+	stomp_frame f = create_frame(data, strlen(data) + 1);
+	
 	CHECK_EQUAL("CONNECT", f->verb);
-	CHECK_EQUAL("", f->body);                
+	CHECK_EQUAL("aaa", f->body);                
 
 	stomp_frame_destroy(f);   	
-	free(b);
 }
+
+void check_null_frame(char *data, int size)
+{
+	CHECK_EQUAL((stomp_frame)NULL, create_frame(data, size));                 
+}
+
+stomp_frame create_frame(char *data, int size) 
+{
+	uint8_t *b = (uint8_t *)malloc(size * sizeof(*b));
+	memcpy(b, data, size);
+
+	stomp_frame f = stomp_frame_create(b, size);
+	free(b);	                                
+
+	return f;
+}                  
+
